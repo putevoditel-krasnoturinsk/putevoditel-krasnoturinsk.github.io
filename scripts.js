45b3a30c86f72e4172c202ee4277ece37785f0b0
maksimemelyanov <maksim.i.emelyanov@gmail.com>
2017-05-12 00:18:28 +0500

    init commit

---
+++ scripts.js
@@ -0,0 +1,24 @@
+<script type="text/javascript">
+	$(document).ready(function () {
+		$("#makeMeScrollable").smoothDivScroll({
+			mousewheelScrolling: "allDirections",
+			manualContinuousScrolling: true,
+			autoScrollingMode: "onStart"
+		});
+	});
+</script>
+
+
+музеи и памятники
+религиозные достопримечательности
+интересные события
+природа
+питание
+турфирмы
+развлечения
+муниципальные сооружения
+
+
+адрес
+телефон
+часы работы
